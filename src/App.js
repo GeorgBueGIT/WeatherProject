ede9692d73d23d92767dfcf56b9ee6eaf6b187d3
GeorgBueGIT <georgbh4@gmail.com>
2022-12-08 16:57:14 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Container className='container'>
+      <Row>
+
+        <div className='weatherData'>
+
+        </div>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Control
+            type="text"
+            id="inputPassword5"
+            aria-describedby="passwordHelpBlock"
+          />
+        </Col>
+        <Col>
+          <Button variant="dark">Primary</Button>
+        </Col>
+      </Row>
+      </Container>
   );
 }
 
